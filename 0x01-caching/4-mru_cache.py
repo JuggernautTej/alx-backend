57ae7e9004d4832713852fc7232802dac6eb1d7b
JuggernautTej <tiwatej@gmail.com>
2024-08-21 14:04:26 +0100

    added task 3 and 4 files

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""This script houses the MRU cache"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """A class that inherits from BaseCaching and implements the MRU cache"""
+    def __init__(self):
+        """Initializes the class"""
+        super().__init__()
+        # The list to track the access order for the LRU
+        self.access_order = []
+
+    def put(self, key, item):
+        """Adds the item to the cache"""
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                # If key is already in the cache, it's position is updated
+                self.access_order.remove(key)
+            elif len(self.cache_data) >= self.MAX_ITEMS:
+                # If the cache is full, the most recently used item is removed
+                mru_key = self.access_order.pop()
+                del self.cache_data[mru_key]
+                print('DISCARD: {}'.format(mru_key))
+            self.cache_data[key] = item
+            self.access_order.append(key)
+
+    def get(self, key):
+        """ This method returns an item by the key and
+        marks it as recently used"""
+        if key is None or key not in self.cache_data:
+            return None
+        # Mark the key as recently used by moving it to the end of access_order
+        self.access_order.remove(key)
+        self.access_order.append(key)
+        return self.cache_data[key]
