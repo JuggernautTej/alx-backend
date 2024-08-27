2a561358d6c4aeff01871f26a71f1f4383183ff7
JuggernautTej <tiwatej@gmail.com>
2024-08-27 15:44:58 +0100

    added setup files and populated tasks 0 and 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""A simple flask app"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """A class to keep track of supported languages"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFALUT_LOCALE = 'en'
+    BABEL_DEFALUT_TIIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """This outputs hello world as a header"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ 0x02-i18n/README.md
@@ -0,0 +1,3 @@
+# 0x02. i18n
+
+This houses the project tasks for the internationalization (I18n) project.
