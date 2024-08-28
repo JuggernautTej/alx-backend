9e51089a7cb69b1fbd7e7fb8faf9ae4d8bd6b151
JuggernautTej <tiwatej@gmail.com>
2024-08-28 11:21:45 +0100

    made pycodestyle correction in task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -28,5 +28,6 @@ def get_locale():
     """Determines the best match for supported languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 if __name__ == '__main__':
     app.run(debug=True)
